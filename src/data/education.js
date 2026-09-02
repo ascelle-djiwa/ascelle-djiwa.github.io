04489a53118add9bf10c8f7c4c2979a8a2223ada
ascelle-djiwa <alaurence.djiwa@gmail.com>
2026-09-02 16:55:03 +0200

    Ajout de la certification sur l'agilité de Product family

--- src/data/education.js
+++ src/data/education.js
@@ -6,40 +6,52 @@ export const EDUCATION = [
     logo: "/images/logo-univ-lille.png", // Chemin à adapter avec ton image réelle
     details: [
       "Données & décisionnel : Base de données Avancées, Entrepôt de données",
-      "Génie logiciel : Applications réparties, ICM, Conception d'Applications",
-      "Ingénierie informatique : Conduite de Projets, Ingénierie des SI",
-      "Gestion & Humain : Recherche Opérationnelle, Analyse Financière, Anglais"
+      "Génie logiciel : Applications réparties et infrastructures, Conception et Paradigmes de Programmations par la Pratique, Conception d'Applications Réparties, ICM (Interactions, Conception, Mobilité)",
+      "Ingénierie informatique : Conduite de Projets, Ingénierie des Systèmes d'Information, Projet Agilité Innovation",
+      "Gestion Informatique : Analyse Financière et Jeu d'Entreprise, Recherche Opérationnelle, Stratégies des entreprises",
+      "Sciences humaines : Anglais, Communication"
     ]
+
   },
+
   {
     id: "inge-1",
     title: "Première année de cycle ingénieur en informatique",
     institution: "3iL Ingénieurs Limoges", 
     logo: "/images/logo-3il.png", 
     details: [
-      "Systèmes & Réseaux : Architecture, Unix, TCP/IP",
-      "Développement : C/C++, Java, UML, Architectures microservices",
-      "Sciences : Probabilités & statistiques, Réseaux de neurones, Analyse du signal"
+      "Systèmes :Architecture des ordinateurs, Architecture & systèmes, Systèmes UNIX, Bases du réseau",
+      "Algorithmique & programmation : Langage C, POO, Structures de données, Fondamentaux du développeur",
+      "Développement logiciel : Bases du web, Méthode UML, Bases de données",
+      "Informatique : COO, Architectures microservices, Fondamentaux en sécurité informatique, Réseaux TCP/IP, C/Unix, Programmation professionnelle",
+      "Sciences de l'ingénieur : Analyse du signal, Probalités & statistiques, Réseaux de neurones, Electronique, Projet",
+      "Sciences humaines : Anglais, Communication, Connaissance de l'entreprise, Economie, Sensibilisation à la sécurité des données, Techniques de gestion des projets, Projet professionnel, Présentation juridique et institutionnel, Transition écologique et développement soutenable"
     ]
   },
+
   {
     id: "prepa-2",
     title: "Deuxième année de classes préparatoires intégrées",
     institution: "Institut Universitaire de la Côte (IUC)",
     logo: "/images/logo-iuc2.png", 
     details: [
-      "Informatique : POO, Java, Linux, Bases de données",
-      "Sciences : Thermodynamique, Électronique, Analyse mathématique"
+      "Mathématiques : Algèbre, Analyse mathématiques",
+      "Sciences de l'ingénieur : Mécanique du solide, Electronique, Thermodynamique II, Electronique, Application numérique",
+      "Informatique : POO, Développement Web, Programmation Java, Système d'exploitation, Système & réseaux, Bases de données, Programmation système Linux",
+      "Sciences humaines :Anglais, Management, Projet culturel, Langue vivante (Chinois), Comptabilité"
     ]
   },
+
   {
     id: "prepa-1",
     title: "Première année de classes préparatoires intégrées",
     institution: "Institut Universitaire de la Côte (IUC)",
     logo: "/images/logo-iuc2.png",
     details: [
-      "Informatique : Algorithmique, C, SQL, Réseaux",
-      "Sciences : Algèbre, Mécanique, Optique, Chimie"
+      "Mathématiques : Algèbre, Analyse mathématiques",
+      "Sciences de l'ingénieur : Mécanique du point, Electricité, Energétique et thermodynamique, Chimie générale, Electronique numérique, TP de Physiques, Optique",
+      "Informatique : EXCEL VBA, Algorithme, Programmation C, Analyse et Conception des Systèmes d'information, Réseaux, Bases de données SQL, Programmation Java",
+      "Sciences humaines : Expression écrite et orale, Anglais, Méthodologie, Projet culturel, Marketing, Communication/Journalisme"
     ]
   }
 ];
@@ -48,5 +60,6 @@ export const CERTIFICATIONS = [
   { id: "cert-1", title: "Introduction à l'ingénierie des données", provider: "Coursera", logo: "/images/logo-coursera.png" },
   { id: "cert-2", title: "Apprendre le python pour l'analyse des données", provider: "OpenClassrooms", logo: "/images/logo-openclassrooms.png" },
   { id: "cert-3", title: "Apprendre l'analyse des données", provider: "LinkedIn Learning", logo: "/images/logo-linkedin-learning.png" },
-  { id: "cert-4", title: "Préparation de carrière dans l'analyse des données", provider: "LinkedIn Learning", logo: "/images/logo-linkedin-learning.png" }
+  { id: "cert-4", title: "Préparation de carrière dans l'analyse des données", provider: "LinkedIn Learning", logo: "/images/logo-linkedin-learning.png" },
+  { id: "cert-5", title: "Scrum & Kanban avec JIRA", provider: "Product Family", logo: "/images/logo-product-family.png" }
 ];
